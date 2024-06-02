5ef2478542e9a165f4b2094e06a17d928b1723a8
Raynara <raynaramartins.1999@gmail.com>
2024-06-02 16:48:59 -0300

    criação do projeto mvc

---
+++ WebApp_EstanteVirtual/WebApp_EstanteVirtual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_EstanteVirtual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
