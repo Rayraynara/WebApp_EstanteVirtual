6337e8d7a282b877303ff6dc916e9b3c9c5db018
Mariana-SN <mary.sutcliff13@gmail.com>
2024-09-16 21:53:15 -0300

    Adicionando sessão de usuário, edição e remoção de conta

--- WebApp_EstanteVirtual/WebApp_EstanteVirtual/Controllers/AccountController.cs
+++ WebApp_EstanteVirtual/WebApp_EstanteVirtual/Controllers/AccountController.cs
@@ -7,11 +7,15 @@ using WebApp_EstanteVirtual.Data;
 using WebApp_EstanteVirtual.Models;
 using Microsoft.EntityFrameworkCore;
 using WebApp_EstanteVirtual.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApp_EstanteVirtual.Controllers
 {
     public class AccountController : Controller
-    {
+    { 
         private readonly ApplicationDbContext _context;
 
         public AccountController(ApplicationDbContext context)
@@ -61,14 +65,29 @@ namespace WebApp_EstanteVirtual.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-             if (ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var user = await _context.Usuarios
                     .FirstOrDefaultAsync(u => u.Email == model.Email);
 
                 if (user != null && IsPasswordValid(user.Senha, model.Password))
                 {
-                    // Definir o cookie de autenticação manualmente
+                    
+                    var claims = new[]
+                    {
+                    new Claim(ClaimTypes.Name, user.Nome),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, user.IsAdmin == true ? "Admin" : "User"),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id)
+                };
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+
+                    
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    
                     HttpContext.Session.SetString("UserId", user.Id);
 
                     return RedirectToAction("Index", "Home");
@@ -78,24 +97,129 @@ namespace WebApp_EstanteVirtual.Controllers
                     ModelState.AddModelError(string.Empty, "Tentativa de login inválida. Verifique os dados e tente novamente.");
                 }
             }
+
             return View(model);
         }
 
-       
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Clear(); 
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditarConta()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            if (userId == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var user = await _context.Usuarios.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EditProfileViewModel
+            {
+                Nome = user.Nome,
+                Email = user.Email,
+                //Senha = user.Senha,
+                Telefone = user.Telefone,
+                Endereco = user.Endereco,
+                CEP = user.CEP,
+                NumeroCartao = user.NumeroCartao
+            };
+
+            return View(model);
+        }
 
         [HttpPost]
-        public IActionResult Logout()
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditarConta(EditProfileViewModel model)
         {
-            HttpContext.Session.Remove("UserId");
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                
+                if (userId == null)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                var user = await _context.Usuarios.FindAsync(userId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.Nome = model.Nome;
+                user.Email = model.Email;
+
+                if (!string.IsNullOrEmpty(model.Telefone))
+                {
+                    user.Telefone = model.Telefone;
+                }
+
+                if (!string.IsNullOrEmpty(model.Endereco))
+                {
+                    user.Endereco = model.Endereco;
+                }
+
+                if (!string.IsNullOrEmpty(model.CEP))
+                {
+                    user.CEP = model.CEP;
+                }
+
+                if (!string.IsNullOrEmpty(model.NumeroCartao))
+                {
+                    user.NumeroCartao = model.NumeroCartao;
+                }
+
+                _context.Usuarios.Update(user);
+                await _context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = "Dados atualizados com sucesso!";
+                return RedirectToAction("EditarConta");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteAccount()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var user = await _context.Usuarios.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(user);
+            await _context.SaveChangesAsync();
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Clear();
+
             return RedirectToAction("Index", "Home");
         }
 
         private bool IsPasswordValid(string storedPassword, string password)
         {
-            if (CryptographyService.EncryptPassword(password).Equals(storedPassword))
-                    return true;
-
-            return false;
+            return CryptographyService.EncryptPassword(password).Equals(storedPassword);
         }
     }
 }
---
+++ WebApp_EstanteVirtual/WebApp_EstanteVirtual/Models/EditProfileViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_EstanteVirtual.Models
+{
+    public class EditProfileViewModel
+    {
+        [Required(ErrorMessage = "O Nome de Usuário é obrigatório.")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "O Nome de Usuário deve ter pelo menos 6 caracteres e no máximo 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O E-mail não é válido.")]
+        public string Email { get; set; }
+
+        /*[Required(ErrorMessage = "A senha é obrigatória.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres e no máximo 100 caracteres.")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "A confirmação da senha é obrigatória.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Senhas não correspondem.")]
+        public string? ConfirmacaoSenha { get; set; }*/
+
+        public string? Telefone { get; set; }
+
+        public string? Endereco { get; set; }
+
+        public string? CEP { get; set; }
+
+        public string? NumeroCartao { get; set; }
+    }
+}
