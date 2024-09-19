8456dad144d26917775b7a60d108ad869fe26b48
Mariana-SN <mary.sutcliff13@gmail.com>
2024-09-19 02:18:37 -0300

    Ajuste no save do preco do livro

--- WebApp_EstanteVirtual/WebApp_EstanteVirtual/Controllers/LivrosController.cs
+++ WebApp_EstanteVirtual/WebApp_EstanteVirtual/Controllers/LivrosController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using WebApp_EstanteVirtual.Data;
 using WebApp_EstanteVirtual.Models;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace WebApp_EstanteVirtual.Controllers
 {
@@ -31,6 +32,14 @@ namespace WebApp_EstanteVirtual.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CadastrarLivro(LivroViewModel model)
         {
+            decimal preco;
+            var precoBruto = Request.Form["Preco"];
+
+            if (decimal.TryParse(precoBruto, NumberStyles.Any, CultureInfo.InvariantCulture, out preco))
+            {
+                model.Preco = preco;
+            }
+
             if (ModelState.IsValid)
             {
                 var livro = new Livro
@@ -43,7 +52,7 @@ namespace WebApp_EstanteVirtual.Controllers
                     Classificacao = model.Classificacao,
                     AnoPublicacao = model.AnoPublicacao,
                     Autor = model.Autor,
-                    Capa = model.Capa 
+                    Capa = model.Capa
                 };
 
                 _context.Livros.Add(livro);
@@ -82,6 +91,14 @@ namespace WebApp_EstanteVirtual.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditarLivro(EditarLivroViewModel model)
         {
+            decimal preco;
+            var precoBruto = Request.Form["Preco"];
+
+            if (decimal.TryParse(precoBruto, NumberStyles.Any, CultureInfo.InvariantCulture, out preco))
+            {
+                model.Preco = preco;
+            }
+
             if (ModelState.IsValid)
             {
                 var livro = await _context.Livros.FindAsync(model.Id);
@@ -106,61 +123,6 @@ namespace WebApp_EstanteVirtual.Controllers
             }
             return View(model);
         }
-
-
-        /*public async Task<IActionResult> EditarLivro(int id)
-        {
-            var livro = await _context.Livros.FindAsync(id);
-            if (livro == null)
-            {
-                return NotFound();
-            }
-
-            var model = new LivroViewModel
-            {
-                Id = livro.Id,
-                Nome = livro.Nome,
-                Preco = livro.Preco,
-                Editora = livro.Editora,
-                QuantidadeEstoque = livro.QuantidadeEstoque,
-                Novo = livro.Novo,
-                Classificacao = livro.Classificacao,
-                AnoPublicacao = livro.AnoPublicacao,
-                Autor = livro.Autor,
-                Capa = livro.Capa 
-            };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditarLivro(LivroViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var livro = await _context.Livros.FindAsync(model.Id);
-
-                if (livro == null)
-                {
-                    return NotFound();
-                }
-
-                livro.Nome = model.Nome;
-                livro.Preco = model.Preco;
-                livro.Editora = model.Editora;
-                livro.QuantidadeEstoque = model.QuantidadeEstoque;
-                livro.Novo = model.Novo;
-                livro.Classificacao = model.Classificacao;
-                livro.AnoPublicacao = model.AnoPublicacao;
-                livro.Autor = model.Autor;
-                livro.Capa = model.Capa; 
-
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(model);
-        }*/
 
         public async Task<IActionResult> RemoverLivro(int id)
         {
